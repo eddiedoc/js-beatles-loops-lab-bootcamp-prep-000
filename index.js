a4d511cd73186d4ba457127baba7ab6c5200b2ce
eddie <psyck_o@hotmail.com>
2018-01-04 05:59:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  
+  for(var i = 0;i <= musicians.length-1; i++) {
+    array[i] = `${musicians[i]} plays ${instruments[i]}`;
+  }
+  
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var array =[];
+  while (i!==facts.length-1) {
+    array[i] = facts[i] +" !!!";
+    i++;
+  }
+  return array;
+}
